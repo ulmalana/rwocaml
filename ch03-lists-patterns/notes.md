e3cc111480f7b88ece535bfd5b686c3cf096317b
rz <muhamaul@gmail.com>
2022-07-05 15:37:41 +0700

    [OCaml] finished chapter 3

--- ch03-lists-patterns/notes.md
+++ ch03-lists-patterns/notes.md
@@ -24,3 +24,51 @@
 ### Detecting errors
 * Pattern matching with `match` can detect **case redundancy** and check for
   its exhaustiveness to ensure that all possible cases will be handled.
+
+## Using the List module effectively
+
+`List` module provides many reusable functions that can be used for computing
+with list, instead of building our own function using pattern matching and
+recursion.
+
+* `concat` is better than `^` for large data.
+* `List.reduce` is a specialized version of `List.fold` that doesnt require
+  explicit starting value.
+* `List.filter_map` is used to both transform and filter some lists. 
+* `List.partition_tf` is used for splitting a list based on some boolean
+  functions
+* `List.append` or `@` are used to concatenate lists
+    * `List.append [1;2;3] [4;5;6]` -> `[1;2;3;4;5;6]`
+    * `[1;2;3] @ [4;5;6]` -> `[1;2;3;4;5;6]`
+
+## Tail recursion
+
+Doing recursion is convenient but it can blows the stack with large amount of
+recursion. Example: computing the length of a list recursively:
+
+```
+    let make_list = List.init n ~f:(fun x -> x)
+    let rec length = function
+        | [] -> 0
+        | _ :: tl -> 1 + length tl
+```
+
+With **tail-call optimization**, we dont need to allocate more stack frame when
+calling a function recursively. The following is the optimized version of
+`length`
+
+```
+    let rec length_plus_n l n = 
+        match l with
+        | [] -> n
+        | _ :: tl -> length_plus_n tl (n+1)
+
+    let length' l = length_plus_n l 0
+```
+
+## Polymorphic comparison
+
+By default, `Base` equality operator is specialize to integers. To use
+polymorphic equality and comparison operators, open module `Base.Poly`. Using
+polymorphic operator might produce **unpredictable behaviour**, so `Base`
+discourages the use of polymorphic operator by hiding it by default.
--- ch03-lists-patterns/source.ml
+++ ch03-lists-patterns/source.ml
@@ -1,3 +1,7 @@
+(* module Sys = Sys_unix
+module Filename = Core.Filename
+*)
+
 (* this function drop a value from a list *)
 let rec drop_value l to_drop = 
     match l with
@@ -35,3 +39,92 @@ let rec sum l =
 let rec sum_if l = 
     if List.is_empty l then 0
     else List.hd_exn l + sum_if (List.tl_exn l)
+
+(* rendering a table from a list *)
+let max_widths header rows =
+    let lengths l = List.map ~f:String.length l in
+    List.fold rows
+      ~init:(lengths header)
+      ~f:(fun acc row ->
+            List.map2_exn ~f:Int.max acc (lengths row))
+
+let render_separator widths =
+    let pieces = List.map widths
+        ~f:(fun w -> String.make w '-')
+    in
+    "|-" ^ String.concat ~sep:"-+-" pieces ^ "-|"
+
+let pad s length =
+    s ^ String.make (length - String.length s) ' ' 
+
+let render_row row widths = 
+    let padded = List.map2_exn row widths ~f:pad in
+    "| " ^ String.concat ~sep:" | " padded ^ " |"
+
+(* concat and ^ does the same job, but for large data concat is better 
+ * because ^ will create new string for every concatenation *)
+let render_table header rows =
+    let widths = max_widths header rows in
+    String.concat ~sep:"\n"
+      (render_row header widths
+        :: render_separator widths
+        :: List.map rows ~f:(fun row -> render_row row widths)
+      )
+
+(* extract file extensions from a list of files *)
+let extensions filenames =
+    List.filter_map filenames ~f:(fun fname ->
+      match String.rsplit2 ~on:'.' fname with
+      | None | Some ("",_) -> None
+      | Some (_, ext) -> Some ext)
+    |> List.dedup_and_sort ~compare:String.compare
+
+(* Splitting a list into ml files and other files *)
+let is_ocaml_source s =
+    match String.rsplit2 s ~on:'.' with
+    | Some (_, ("ml"|"mli")) -> true
+    | _ -> false
+
+let (ml_files, other_files) = 
+  List.partition_tf ["foo.c"; "foo.ml"; "bar.ml"; "haha.hs"] ~f:is_ocaml_source
+
+(* listing a directory
+ * currently doesnt works since because of Core.Sys deprecation
+let rec ls_rec s =
+    if Sys.is_file_exn ~follow_symlinks:true s
+    then [s]    
+    else
+        Sys.ls_dir s
+        |> List.map ~f:(fun sub -> ls_rec (Filename.concat s sub))
+        |> List.concat
+        (* or like this 
+         * Sys.ls_dir s
+         * |> List.concat_map ~f:(fun sub -> ls_rec (Filename.concat s sub)) 
+         * *)
+*)
+
+(* computing the length of list: unoptimized and tail-call optimized *)
+
+let make_list n = List.init n ~f:(fun x -> x)
+
+let rec length = function
+    | [] -> 0
+    | _ :: tl -> 1 + length tl
+
+(* optimized *)
+let rec length_plus_n l n =
+    match l with
+    | [] -> n
+    | _ :: tl -> length_plus_n tl (n+1)
+
+let length' l = length_plus_n l 0
+
+(* recreating terser version remove_sequential_duplicates function
+ * from chapter 1.
+ * this only works with integer *)
+let rec remove_sequential_duplicates list =
+    match list with
+    | [] | [_] as l -> l
+    | first :: (second :: _ as tl) when first = second ->
+        remove_sequential_duplicates tl
+    | first :: tl -> first :: remove_sequential_duplicates tl
